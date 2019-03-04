225cbdf541f3eb267187fed7bd2d4d268f750a91
Omar Ghanem <ghanem374@gmail.com>
2019-03-04 17:45:31 +0200

    Add files via upload

--- ChipSBqM/Counter.v
+++ ChipSBqM/Counter.v
@@ -1,15 +1,17 @@
-module counter(CO,UP,DOWN,CLK,MR,);
+module counter(CO,EF,FF,UP,DOWN,CLK,MR);
 input UP,DOWN,CLK,MR ;
 
 parameter H=3'b000,G=3'b001,F=3'b010,E=3'b011,D=3'b100,C=3'b101,B=3'b110,A=3'b111 ;
 reg[3:0] CS ;reg[3:0] NS ;
 output reg[2:0] CO ;
-
+output reg EF,FF ;
 
 initial
 begin 
 CS <= H ;
 CO = 0;
+EF = 1 ;
+FF = 0 ;
 end
 
 
@@ -55,36 +57,44 @@ end
 
 always@(posedge CLK)
 begin
-if (MR==0)begin CS <= NS ;  end
-else begin  CS <=H ; end
+if (MR==0)begin CS <= NS ; 
+
+ end
+else begin  CS <=H ;
+EF = 1 ; FF = 0 ;
+
+ end
 
 
 case(CS)
 A:begin
- CO = 7 ;
+ CO <= 7 ;
   end// 7 
 
-B:begin CO = 6 ;
+B:begin  CO <= 6 ;
  end//6
 
-C: begin CO = 5 ;
+C: begin  CO <= 5 ;
  end //5
 
-D: begin CO = 4 ;
+D: begin  CO <= 4 ;
 end //4
 
-E:begin CO = 3 ;
+E:begin  CO <= 3 ;
 end//3
 
-F:begin CO = 2 ;
+F:begin  CO <= 2 ;
   end//2
-G:begin CO = 1 ;
-  end//1
-H: begin CO = 0 ;
+G:begin CO <= 1 ; end //1
+H: begin  CO <= 0 ;
  end
  
 endcase 
 
+if(CO == 0)begin EF = 1 ; end
+	else begin EF = 0 ; end
+	if(CO == 7)begin FF = 1 ; end
+	else begin FF = 0 ; end
 end
 
 
