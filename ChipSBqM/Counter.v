7406705f8347b3656160aba1cac5cd163e118619
Omar Ghanem <ghanem374@gmail.com>
2019-03-04 16:38:16 +0200

    Add files via upload

--- ChipSBqM/Counter.v
+++ ChipSBqM/Counter.v
@@ -1,13 +1,15 @@
-module counter(CO2,CO1,CO,UP,DOWN,CLK,MR);
+module counter(CO,UP,DOWN,CLK,MR,);
 input UP,DOWN,CLK,MR ;
-output reg  CO2,CO1,CO ; 
-parameter A=3'b000,B=3'b001,C=3'b010,D=3'b011,E=3'b100,F=3'b101,G=3'b110,H=3'b111 ;
+
+parameter H=3'b000,G=3'b001,F=3'b010,E=3'b011,D=3'b100,C=3'b101,B=3'b110,A=3'b111 ;
 reg[3:0] CS ;reg[3:0] NS ;
+output reg[2:0] CO ;
 
 
 initial
 begin 
-CS <= A ;
+CS <= H ;
+CO = 0;
 end
 
 
@@ -59,54 +61,33 @@ else begin  CS <=H ; end
 
 case(CS)
 A:begin
- CO2=1 ; CO1=1 ;CO= 1; end// 7 
+ CO = 7 ;
+  end// 7 
 
-B:begin
- CO2= 1;CO1= 1;CO= 0;end //6
+B:begin CO = 6 ;
+ end//6
 
-C: begin
-CO2= 1; CO1= 0;CO= 1; end //5
+C: begin CO = 5 ;
+ end //5
 
-D: begin 
-CO2= 1; CO1=0 ;CO= 0;end //4
+D: begin CO = 4 ;
+end //4
 
-E:begin
- CO2=0 ; CO1=1 ;CO= 1;end//3
+E:begin CO = 3 ;
+end//3
 
-F:begin
- CO2=0 ;CO1= 1;CO= 0; end//2
-G:begin
- CO2=0 ;CO1= 0;CO=1 ; end//1
-H: begin
-CO2=0 ; CO1=0 ;CO= 0; end
+F:begin CO = 2 ;
+  end//2
+G:begin CO = 1 ;
+  end//1
+H: begin CO = 0 ;
+ end
  
 endcase 
 
 end
 
-/**always@(CS)
-begin
 
-case(CS)
-A: C2=1;// C1=1;C0= 1;
-
-B: C2 = 1; //C1 = 1;C0= 0;
-C: C2 = 1;// C1 = 0;C0= 1;
-
-D: C2 = 1; //C1 =0 ;C0= 0;
-
-E: C2 =0 ; //C1 =1 ;C0= 1;
-
-F: C2 =0 ; //C1 = 1;C0= 0;
-
-G: C2 =0 ; C1 = 0;C0=1 ;
-
-H: C2 =0 ; C1 =0 ;C0= 0;
- 
-endcase 
-
-
-end**/
 
 
 
